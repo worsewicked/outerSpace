1c1015bf77c753dbdca19e79aac0da3c50746b68
Michael Curry <worsewicked@yahoo.co.uk>
2012-01-11 16:59:27 +0000

    Update 3, added powerups and ship health. New Graphics.

--- src/outerSpace/OuterSpace.java
+++ src/outerSpace/OuterSpace.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 @SuppressWarnings("serial")
@@ -17,8 +18,9 @@ public class OuterSpace extends GraphicsProgram implements KeyListener{
 			outerSpace.start(args);
 			
 	}	
+	@SuppressWarnings("static-access")
 	public void run() {
-		
+
 		//-- Setting the size of the screen --//
 		setSize(1000, 600);
 		//-----------------------------------//
@@ -89,6 +91,48 @@ public class OuterSpace extends GraphicsProgram implements KeyListener{
 		t3.start();
 		//-----------------------------------//
 		
+		//-- Fourth runnable for powerUps --//
+		Runnable r4 = new Runnable() {
+		
+			public void run() {
+				
+				powerUps();
+				
+			}
+		
+		};
+		Thread t4 = new Thread(r4);
+		t4.start();
+		//---------------------------------//
+		
+		//-- Fifth runnable for health --//
+		Runnable r5 = new Runnable() {
+			
+			public void run() {
+				
+				updateHealth();
+				
+			}
+		
+		};
+		Thread t5 = new Thread(r5);
+		t5.start();
+		//---------------------------------//
+		
+		//-- Sixth runnable for resetStats --//
+		Runnable r6 = new Runnable() {
+			
+			public void run() {
+				
+				resetStats();
+				
+			}
+		
+		};
+		Thread t6 = new Thread(r6);
+		t6.start();
+		//---------------------------------//
+		
 	}
 	public void init() {
 		
@@ -97,59 +141,96 @@ public class OuterSpace extends GraphicsProgram implements KeyListener{
 	}
 
 	public void animateAsteroid(Asteroid asteroid) {
-		
-		while(asteroid.getY() + asteroid.getHeight() < 800) {
-			
-			if(asteroid.getBounds().intersects(ship.getBounds()) || ship.getBounds().intersects(asteroid.getBounds())) {
+
+		while(true) {
+			asteroid.move(deltaX, deltaY);
+
+			if(asteroid.getBounds().intersects(ship.getBounds())) {
+
 				
-				asteroid.setLocation(Math.random() * 750, Math.random() * 250);
+				asteroid.setLocation(Math.random() * Math.random() * 600, Math.random() * 400);
 				time += 1;
 				points += 1;
-			
+				health -= 5;
+
 			}
-			
-			asteroid.move(0, 10);
+			if(bullet.getBounds().intersects(asteroid.getBounds())) {
+				
+				asteroid.setLocation(Math.random() * Math.random() * 600, Math.random() * 400);
+				points += 2;
+				time += 2;
+				
+			}
+
 			pause(5);
+
+			if(asteroid.getY() < 0)
+				deltaY = 10;
+
+			else if(asteroid.getY() + asteroid.getHeight() > getSize().getHeight())
+				deltaY = -10;
+
+			if(asteroid.getX() < 0)
+				deltaX = 1;
+
+			else if(asteroid.getX() + asteroid.getWidth() > getSize().getWidth())
+				deltaX = -1;
 			
-			if(asteroid.getY() + asteroid.getHeight() > 800) {
+			if(ship.getHeight() + ship.getY() < 50) {
 				
-				while(asteroid.getY() + asteroid.getHeight() > 0) {
+				ship.setLocation(ship.getX(), ship.getY() + 5);
 				
-					asteroid.move(0, -10);
-					pause(5);
-					
-					if(asteroid.getBounds().intersects(ship.getBounds()) || ship.getBounds().intersects(asteroid.getBounds())) {
-						
-						asteroid.setLocation(Math.random() * 750, Math.random() * 250);
-						time += 1;
-						points += 1;
-					
-					}
-					
-					if(asteroid.getY() + asteroid.getHeight() == 0) {
-						
-						break;
-						
-					}
-				}
+			}
+			if(ship.getWidth() + ship.getX() < 50) {
+				
+				ship.setLocation(ship.getX() + 5, ship.getY());
+				
+			}
+			if(ship.getHeight() + ship.getY() > 750) {
+				
+				ship.setLocation(ship.getX(), ship.getY() - 5);
+				
+			}
+			if(ship.getHeight() + ship.getX() > 950) {
+				
+				ship.setLocation(ship.getX() - 5, ship.getY());
+				
 			}
 		}
 	}
 	
-	@SuppressWarnings("deprecation")
+	public void updateHealth() {
+		
+		while(health > -1) {
+			if(health <= 0) {
+				
+				finalScore.setLabel("Final score: " + points);
+				finalScore.setColor(Color.WHITE);
+				sOQ.setColor(Color.WHITE);
+				add(finalScore);
+				add(sOQ);
+				
+			}
+		}
+		pause(1);
+		
+		
+	}
+	
 	public void updateTime() {
 		
 		while(time > 0) {
 			time -= 1;
 			timeLbl.setLabel("Time: " + time);
 			pause(1000);
-		}
-		if(time == 0) {
+			healthLbl.setLabel("Health: " + health);
 			
-			GLabel sOQ = new GLabel("Would you like to start over? Press L To Quit or K to Start Over", 375, 400);
-			sOQ.setColor(Color.WHITE);
-			sOQ.setFont(new Font("Sans Serif", Font.BOLD, 18));
-			add(sOQ);
+			if(time <= 0 || health <= 0) {
+				
+				sOQ.setColor(Color.WHITE);
+				add(sOQ);
+				
+			}
 			
 		}
 		
@@ -173,29 +254,149 @@ public class OuterSpace extends GraphicsProgram implements KeyListener{
 		
 	}
 	
+	
+	public void powerUps() {
+		
+		GOval timePlus = new GOval(100, 100, Math.random() * 200, Math.random() * 200);
+		GOval pointsPlus = new GOval(100, 100, Math.random() * 200, Math.random() * 200);
+		GOval speedPlus = new GOval(100, 100, Math.random() * 200, Math.random() * 200);
+		GOval timeMinus = new GOval(100, 100, Math.random() * 200, Math.random() * 200);
+		GOval pointsMinus = new GOval(100, 100, Math.random() * 200, Math.random() * 200);
+		GOval speedMinus = new GOval(100, 100, Math.random() * 200, Math.random() * 200);
+		while(time > 0) {
+			long rand = Math.round(Math.random() * 100);
+			pause(1000);
+			if(rand == 25) {
+				
+				timePlus.setFillColor(Color.GREEN);
+				timePlus.setFilled(true);
+				add(timePlus);
+				
+			}
+			if(rand == 35) {
+				
+				pointsPlus.setFillColor(Color.GREEN);
+				pointsPlus.setFilled(true);
+				add(pointsPlus);
+				
+			}
+			if(rand == 45) {
+				
+				speedPlus.setFillColor(Color.GREEN);
+				speedPlus.setFilled(true);
+				add(speedPlus);
+				
+			}
+			if(rand == 55) {
+				
+				timeMinus.setFillColor(Color.RED);
+				timeMinus.setFilled(true);
+				add(timeMinus);
+				
+			}
+			if(rand == 65) {
+				
+				pointsMinus.setFillColor(Color.RED);
+				pointsMinus.setFilled(true);
+				add(pointsMinus);
+				
+			}
+			if(rand == 75) {
+				
+				speedMinus.setFillColor(Color.RED);
+				speedMinus.setFilled(true);
+				add(speedMinus);
+				
+			}
+			if(ship.getBounds().intersects(timePlus.getBounds())) {
+				
+				time += 5;
+				remove(timePlus);
+				
+			}
+			if(ship.getBounds().intersects(pointsPlus.getBounds())) {
+				
+				points += 3;
+				remove(pointsPlus);
+				
+			}
+			if(ship.getBounds().intersects(speedPlus.getBounds())) {
+				
+				speed += 5;
+				remove(speedPlus);
+				
+			}
+			if(ship.getBounds().intersects(timeMinus.getBounds())) {
+				
+				time -= 5;
+				remove(timeMinus);
+				
+			}
+			if(ship.getBounds().intersects(pointsMinus.getBounds())) {
+				
+				points -= 3;
+				remove(pointsMinus);
+				
+			}
+			if(ship.getBounds().intersects(speedMinus.getBounds())) {
+				
+				speed -= 5;
+				remove(speedMinus);
+				
+			}
+	
+		}
+		
+	}
+	
+	public void resetStats() {
+		
+		remove(sOQ);
+		remove(finalScore);
+		time = 10;
+		points = 0;
+		health = 100;
+		speed = 10;
+		
+	}
+	
+	public int speed = 10;
 	public void keyPressed(final KeyEvent e) {
 		
 		int key = e.getKeyCode();
+		if(key == KeyEvent.VK_K) {
+			
+			resetStats();
+			
+		}
 		if(key == KeyEvent.VK_UP) {
 			
-			ship.move(0, -10);
+			ship.move(0, -speed);
+			ship.setImage("shipUp.jpg");
+			ship.setSize(63, 183);
 			pause(20);
 			
 		}
 		if(key == KeyEvent.VK_DOWN) {
 			
-			ship.move(0, 10);
+			ship.move(0, speed);
+			ship.setImage("shipDown.jpg");
+			ship.setSize(64, 184);
 			pause(20);
 			
 		}
 		if(key == KeyEvent.VK_LEFT) {
 			
-			ship.move(-10, 0);
+			ship.move(-speed, 0);
+			ship.setImage("ship.jpg");
+			ship.setSize(185, 65);
 			
 		}
 		if(key == KeyEvent.VK_RIGHT) {
 			
-			ship.move(10, 0);
+			ship.move(speed, 0);
+			ship.setImage("shipRight.jpg");
+			ship.setSize(186, 66);
 			
 		}
 		if(key == KeyEvent.VK_L) {
@@ -203,12 +404,9 @@ public class OuterSpace extends GraphicsProgram implements KeyListener{
 			System.exit(0);
 			
 		}
-		if(key == KeyEvent.VK_K) {
+		if(key == KeyEvent.VK_C) {
 			
-			time = 10;
-			points = 0;
-			remove(finalScore);
-			
+			add(Cat);
 			
 		}
 
@@ -223,19 +421,26 @@ public class OuterSpace extends GraphicsProgram implements KeyListener{
 	//----------------------------//
 	
 	//-- Declaring stats variables --//
-	int points = 0;
-	int time = 10;
-	int health = 100;
+	public int points = 0;
+	public int time = 10;
+	public int health = 100;
 	//------------------------------//
 	
 	//-- Creating an instance of a bullet --//
 	GImage bullet = new GImage("bullet.jpg", ship.getX() + 50, ship.getY() + 25);
 	//--------------------------------------//
 	
+	//-- Asteroid motion --//
+	int deltaX = 1;
+	int deltaY = 10;
+	//---------------------//
+	
 	//-- Declaring stats Labels --//
 	GLabel pointsLbl = new GLabel("Points: " + points, 0, 15);
 	GLabel timeLbl = new GLabel("Time: " + time, 0, 30);
 	GLabel healthLbl = new GLabel("Health: " + health, 0, 45);
 	GLabel finalScore = new GLabel(" ", 300, 300);
+	GLabel sOQ = new GLabel("Would you like to start over? Press L To Quit or K to Start Over", 300, 320);
+	GLabel Cat = new GLabel("Cat", 300, 400);
 	//---------------------------//
 }
